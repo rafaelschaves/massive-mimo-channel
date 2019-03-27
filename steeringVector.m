4f9a6d892b91f67d09c2c8c1668c14b6818fff3c
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-03-27 15:28:45 +1100

    Some details in the small-scale fading genaration were fixed and some comments were added in the help of this function

--- steeringVector.m
+++
@@ -1,9 +0,0 @@
-function steering_vector = steeringVector(M,theta,d,lambda)
-
-m   = (0:M-1).';
-tau = d*sin(theta)/lambda;
-Tau = repmat(tau',M,1);
-
-steering_vector = exp(-2*pi*1i*Tau.*repmat(m,1,size(theta,1)));
-
-end
