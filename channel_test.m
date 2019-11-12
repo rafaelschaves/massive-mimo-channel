6d3db3a759bb9611e8b1d79c6aa5534e2a21528c
Rafael Chaves <rflschaves@poli.ufrj.br>
2019-11-13 10:30:39 +1100

    Remove the example related to the sparse option

--- channel_test.m
+++ channel_test.m
@@ -10,13 +10,10 @@ commcell.nUsers          = K;                                              % Num
 commcell.radius          = 500;                                            % Cell's raidus (circumradius) in meters
 commcell.bsHeight        = 30;                                             % Height of base station in meters
 commcell.userHeight      = [1 2];                                          % Height of user terminals in meters ([min max])
-commcell.nPaths          = 1;                                              % Number of Multipaths
 commcell.frequency       = 2e9;                                            % Carrier frequency in Hz 
 commcell.meanShadowFad   = 0;                                              % Shadow fading mean in dB
 commcell.stdDevShadowFad = 8;                                              % Shadow fading standard deviation in dB
 commcell.city            = 'large';                                        % Type of city
 
-[H_ra, beta_ra] = massiveMIMOChannel(commcell,'rayleigh');
-[H_ur, beta_ur] = massiveMIMOChannel(commcell,'ur-los');
-[H_sp, beta_sp] = massiveMIMOChannel(commcell,'sparse');
-    
+[G_ra, beta_ra] = massiveMIMOChannel(commcell,'rayleigh');
+[G_ur, beta_ur] = massiveMIMOChannel(commcell,'ur-los');
